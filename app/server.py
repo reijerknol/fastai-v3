26be199b9887428f3bb3dd8a62bf7a355e070b3c
rknol <rknol@deloitte.nl>
2019-11-12 14:49:50 +0100

    changed dependencies, changed model

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/d4of3n49kuroqdy/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/v5pkmwx58011aod/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['Toilet paper dispenser',
